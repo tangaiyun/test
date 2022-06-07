67c27624c2bdd4c6b66ebae9e3f14fcce0e336ad
Aiyun Tang <15520929@qq.com>
2022-06-07 13:09:38 +0800

    Add files via upload

---
+++ test/Main.java
@@ -0,0 +1,9 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Main {
+
+    public static void main(String[] args) throws UnknownHostException {
+        System.out.println(InetAddress.getLocalHost().getHostName());
+    }
+}
